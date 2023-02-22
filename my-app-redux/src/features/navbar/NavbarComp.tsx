e0de648517f2842b5b4d677b0b64bae02e6f3117
Davide D'Agostino <davide-d.agostino@outlook.com>
2023-02-22 19:29:19 +0100

    agg. modifiche navbar

--- my-app-redux/src/features/navbar/NavbarComp.tsx
+++ my-app-redux/src/features/navbar/NavbarComp.tsx
@@ -7,6 +7,10 @@ import { useAppSelector } from '../../app/hooks';
 import { selectIsLoggedIn, logout } from '../login/userLoginSlice';
 import { selectUserLogin } from '../login/userLoginSlice';
 
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
 function NavbarComp() {
  
   const users = useAppSelector(selectUserLogin); 
@@ -20,12 +24,18 @@ function NavbarComp() {
   const isLoggedIn = useAppSelector(selectIsLoggedIn);
 
   function userLogout() {
+    handleClose()
     dispatch(logout())
     navigate('/login')
   }
 
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   return (
+    <>
     <Navbar className="nav-cust" collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
         <Link to="/" className='nav-link logo fs-5'>TryList App</Link>
@@ -41,13 +51,29 @@ function NavbarComp() {
             </Nav> 
             : 
             <Nav>
-                <Nav.Link className='nav-link name-profile-nav'>Hello {name}</Nav.Link>
-                <Nav.Link className="nav-link" onClick={userLogout}>Logout</Nav.Link>
+                <Link to="/profile" className='nav-link name-profile-nav'>Hello {name}</Link>
+                <Nav.Link className="nav-link" onClick={handleShow}>Logout</Nav.Link>
             </Nav> 
           }  
         </Navbar.Collapse>
       </Container>
     </Navbar>
+
+    <Modal show={show} onHide={handleClose} backdrop="static" keyboard={false}>
+        <Modal.Header closeButton>
+          <Modal.Title>Logout TryList App</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo,te ne vai senza salutare? A presto!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Rimango
+          </Button>
+          <Button variant="primary" onClick={userLogout}>
+            Logout
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 }
 
