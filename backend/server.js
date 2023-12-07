8c3ae9ea29efb3728a5ae6a24813b441f1428f7b
Davide D'Agostino <davide-d.agostino@outlook.com>
2023-12-07 16:43:10 +0100

    sv

--- backend/server.js
+++ backend/server.js
@@ -14,12 +14,7 @@ const cors = require('cors');
 app.use(express.json())
 
 
-app.use(cors({
-    origin: 'https://todo-list-app-davdag.netlify.app', // Sostituisci con l'URL effettivo del tuo frontend su Netlify
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, // Consente l'invio dei cookie
-    optionsSuccessStatus: 204, // Imposta il codice di risposta per le richieste Preflight su 204
-  }));
+app.use(cors());
   
 
 
